c03f91b8266c20b400ee6a52175fcd3872eca806
Ved Rathi <65692297+Ved-programmer@users.noreply.github.com>
2020-12-26 09:54:57 +0530

    Update utility.py

--- TypingPro/utility.py
+++ TypingPro/utility.py
@@ -27,8 +27,7 @@ class button():
     def isOver(self, pos):
         #Pos is the mouse position or a tuple of (x,y) coordinates
         if pos[0] > self.x and pos[0] < self.x + self.width:
-            if pos[1] > self.y and pos[1] < self.y + self.height:
-                return True
+            return pos[1] > self.y and pos[1] < self.y + self.height
             
         return False
 
